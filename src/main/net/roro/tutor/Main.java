1a20ac71fa30b9ddcfd0037f99ac79e93f9d063b
Rohit Srinivasan <thisisroroiamro@gmail.com>
2020-05-28 06:26:48 -0700

    New changes

--- src/main/net/roro/tutor/Main.java
+++ src/main/net/roro/tutor/Main.java
@@ -1,10 +1,12 @@
 package net.roro.tutor;
 
 import java.util.Arrays;
+import org.apache.commons.lang3.StringUtils;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
+        // System.out.println(Arrays.toString(args));
+        System.out.println(StringUtils.swapCase(Arrays.toString(args)));
     }
 }
